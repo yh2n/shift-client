1577366f13c95fcb089a9076d8224aea6b3c8fee
yohann <yohann@yohanns-iMac.home>
2018-10-23 15:42:49 -0400

    First commit

---
+++ config.js
@@ -0,0 +1,6 @@
+export const API_BASE_URL =
+    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:8080';
+
+export default SERVER_URL;
