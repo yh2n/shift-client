429056fe56f62b2fd9efebd3808c224e1848598c
yh2n <yohann.potico@gmail.com>
2019-03-27 21:38:30 -0400

    Organize imports and pass props to <AdmiEmployeeDashboard />

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import HomePage from './components/HomePage';
-import AdminLogin from './components/AdminLogin';
-import AuthenticationPage from './components/AuthenticationPage';
-import RegistrationPage from './components/RegistrationPage';
-import AdminRegistrationPage from './components/AdminRegistrationPage';
-import AdminSchedule from './components/AdminSchedule';
-import UserAccount from './components/UserAccount';
-import AvailabilityPage from './components/AvailabilityPage';
-import UserSchedule from './components/UserSchedule';
-import Contacts from './components/Contacts';
-import ContactAvailabilityPage from './components/ContactAvailabilityPage';
-import RegisterEmployee from './components/RegisterEmployee';
 import AdminAccount from './components/AdminAccount';
 import AdminEmployeeDashboard from './components/AdminEmployeeDashboard';
+import AdminLogin from './components/AdminLogin';
+import AdminRegistrationPage from './components/AdminRegistrationPage';
+import AdminSchedule from './components/AdminSchedule';
+import AuthenticationPage from './components/AuthenticationPage';
+import AvailabilityPage from './components/AvailabilityPage';
+import Contacts from './components/Contacts';
+import ContactAvailabilityPage from './components/ContactAvailabilityPage';
 import EmployeeAvailabilityPage from './components/EmployeeAvailabilityPage';
+import HomePage from './components/HomePage';
+import LandingPage from './components/LandingPage';
 import NotFound from './components/NotFound';
+import RegistrationPage from './components/RegistrationPage';
+import RegisterEmployee from './components/RegisterEmployee';
+import UserAccount from './components/UserAccount';
+import UserSchedule from './components/UserSchedule';
 
 import './App.css';
 
@@ -40,7 +40,7 @@ class App extends Component {
               <Route exact path="/admin" component={AdminSchedule} />
               <Route exact path="/admin/register_employee" component={RegisterEmployee} />
               <Route exact path="/admin/employee_database" component={AdminAccount} />
-              <Route exact path="/admin/employee/:id" component={AdminEmployeeDashboard} />
+              <Route exact path="/admin/employee/:id" render={props => <AdminEmployeeDashboard {...props}/>} />
               <Route exact path="/admin/employee-availability/:id" component={EmployeeAvailabilityPage} />
               <Route path=" * " component={NotFound} />
             </div>
