08d62fd21361dc8f6f74c897fe6c51c4987e6500
yh2n <yohann.potico@gmail.com>
2019-05-21 15:08:05 -0400

    Change props for default value with ScheduleSelect component

--- src/components/AdminSchedule.js
+++ src/components/AdminSchedule.js
@@ -387,7 +387,7 @@ export class Schedule extends Component {
                     <PositionRow key="schedule_bussers" position="Bussers"/>
                     {busserRow}
                 </div>
-                <ScheduleSelect value={this.state.selectedWeek} onChange={this.handleScheduleSelection}/>
+                <ScheduleSelect selectedWeek={this.state.selectedWeek} onChange={this.handleScheduleSelection}/>
                 <button onClick={this.incrementCountProps}>Save</button>
                 <div className="admin_schedule_container">
                     <SelectedWeekDayRow className="day_row" selectedWeek={this.state.selectedWeek}/>
