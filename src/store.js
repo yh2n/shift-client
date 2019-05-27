16f439f849d61deadd58f9d12288fd7e678661f4
yh2n <yohann.potico@gmail.com>
2019-05-27 11:51:06 -0400

    Remove unused import

--- src/store.js
+++ src/store.js
@@ -1,17 +1,13 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 import { loadAuthToken } from './local_storage';
 import authReducer from '../src/reducers/auth';
 import protectedDataReducer from '../src/reducers/protected_data';
 import employeeReducer from '../src/reducers/fetch_employees';
-//import UserInfoReducer from './reducers/fetch_employee_info';
 import scheduleReducer from '../src/reducers/set_schedule';
 import { setAuthToken,refreshAuthToken } from '../src/actions/auth';
 
-// const enhancers = compose(
-//     window.__REDUX_DEVTOOLS_EXTENSION__ / window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION__ / window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : f => f
-// );
 
 
 const store = createStore(
@@ -23,7 +19,6 @@ const store = createStore(
         schedule: scheduleReducer,
     }), 
     applyMiddleware(thunk), 
-    // enhancers
 );
 
 const authToken = loadAuthToken();
