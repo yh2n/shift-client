dd02cfed4b64fd6f188a66d6dfa22220398e6479
yh2n <yohann.potico@gmail.com>
2019-05-28 13:50:01 -0400

    Create month row for dynamic change with select component

---
+++ src/components/SelectedMonthRow.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import moment from 'moment';
+
+
+ const SelectedMonthRow = props => {
+     let year = moment().get('year')
+    return (
+        <>
+            <div className={props.className}>
+                <div className="month-selected">{moment(`${year}`).add(`${props.selectedWeek}`, 'weeks').format("MMMM YYYY").toUpperCase()}</div>
+            </div>
+        </>
+    )
+}
+
+
+export default SelectedMonthRow;
