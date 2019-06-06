e5ae75a6fc7670ea78f01972e9c0329f15b407da
yh2n <yohann.potico@gmail.com>
2019-06-06 15:00:09 -0400

    Add  page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import moment from 'moment';
 
 import AdminAccount from './components/AdminAccount';
@@ -37,28 +37,30 @@ moment.updateLocale('en', {
 class App extends Component {
   render() {
     return (
-          <Router>
+        <Router>
             <div className="app">
-              <Route exact path="/" component={LandingPage} />
-              <Route exact path="/login" component={HomePage} />
-              <Route exact path="/admin/login" component={AdminLogin} />
-              <Route exact path="/verification" component={AuthenticationPage} />
-              <Route exact path="/registration" component={RegistrationPage} />
-              <Route exact path="/admin/registration" component={AdminRegistrationPage} />
-              <Route exact path="/my_account/:username" component={UserAccount} />
-              <Route exact path="/my_account/:username/profile" component={UserAccount} />
-              <Route exact path="/my_account/:username/availability" component={AvailabilityPage} />
-              <Route exact path="/my_account/:username/schedule" component={UserSchedule} />
-              <Route exact path="/my_account/:username/contacts" component={Contacts} />
-              <Route exact path="/my_account/:username/contact-availability/:id" component={ContactAvailabilityPage} />
-              <Route exact path="/admin" component={AdminSchedule} />
-              <Route exact path="/admin/register_employee" component={RegisterEmployee} />
-              <Route exact path="/admin/employee_database" component={AdminAccount} />
-              <Route exact path="/admin/employee/:id" render={props => <AdminEmployeeDashboard {...props}/>} />
-              <Route exact path="/admin/employee-availability/:id" component={EmployeeAvailabilityPage} />
-              <Route path=" * " component={NotFound} />
+              <Switch>
+                <Route exact path="/" component={LandingPage} />
+                <Route exact path="/login" component={HomePage} />
+                <Route exact path="/admin/login" component={AdminLogin} />
+                <Route exact path="/verification" component={AuthenticationPage} />
+                <Route exact path="/registration" component={RegistrationPage} />
+                <Route exact path="/admin/registration" component={AdminRegistrationPage} />
+                <Route exact path="/my_account/:username" component={UserAccount} />
+                <Route exact path="/my_account/:username/profile" component={UserAccount} />
+                <Route exact path="/my_account/:username/availability" component={AvailabilityPage} />
+                <Route exact path="/my_account/:username/schedule" component={UserSchedule} />
+                <Route exact path="/my_account/:username/contacts" component={Contacts} />
+                <Route exact path="/my_account/:username/contact-availability/:id" component={ContactAvailabilityPage} />
+                <Route exact path="/admin" component={AdminSchedule} />
+                <Route exact path="/admin/register_employee" component={RegisterEmployee} />
+                <Route exact path="/admin/employee_database" component={AdminAccount} />
+                <Route exact path="/admin/employee/:id" render={props => <AdminEmployeeDashboard {...props}/>} />
+                <Route exact path="/admin/employee-availability/:id" component={EmployeeAvailabilityPage} />
+                <Route component={NotFound} />
+              </Switch>
             </div>
-          </Router>
+        </Router>
     );
   }
 }
