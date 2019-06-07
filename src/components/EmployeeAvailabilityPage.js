52059e6690f67d44a2df50d5c3f97000d4582589
yh2n <yohann.potico@gmail.com>
2019-06-07 14:18:21 -0400

    Change staff availability component

--- src/components/EmployeeAvailabilityPage.js
+++ src/components/EmployeeAvailabilityPage.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import AccountNav from './AccountNav';
 import AdminMenuModal from './AdminMenuModal';
-import EmployeeAvailability from './EmployeeAvailability';
-import { Link } from 'react-router-dom';
+import ContactAvailability from './ContactAvailability';
+
 
 export default class EmployeeAvailabilityPage extends Component {
     constructor(props) {
 		super(props);
 
-		this.state = {
-			isOpen : false,
-		}
+		this.state = { isOpen : false }
     }	
     
     toggleModal = () => {
-		this.setState({
-			isOpen: !this.state.isOpen
-		});
+		this.setState({ isOpen: !this.state.isOpen });
     }
     
     render() {
@@ -27,10 +23,10 @@ export default class EmployeeAvailabilityPage extends Component {
 					show={this.state.isOpen}
                     onClose={this.toggleModal}
 				/>
-                <EmployeeAvailability {...this.props} />
-                <Link to={`/admin/employee_database`} style={{ textDecoration: 'none' }}>
-                    <button>Back to Contacts</button>
-                </Link>
+                <ContactAvailability 
+                    {...this.props}
+                    linkTo={`/admin/employee_database`} 
+                />
             </div>
         )
     }
