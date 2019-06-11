dccd295ab3ab6b732f3d0d2564f8be442dae4c51
yh2n <yohann.potico@gmail.com>
2019-06-11 15:49:11 -0400

    Change background color and background img size

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import LandingPageIcons from './LandingPageIcons';
+import Background1 from '../images/blurred-background-calendar-cellphone-1893424.jpg'
 
 import './LandingPage.css';
 
@@ -7,33 +9,18 @@ export default class LandingPage extends Component {
     state = {  }
     render() {
         return (
-            <>
-                <div className="landing_page">
-                    Welcome to Shift!!
+            <div className="landing_page">
+                <p>shift</p>
+                <div className="background_images">
+                    <img src={Background1} alt="" width="100%" height="100%"/>
                 </div>
                 <div className="instructions">
-                    <p>
-                        Check the demo!
-                    </p>  
-                    <p>
-                    "On the other hand, we denounce with righteous 
-                    indignation and dislike men who are so beguiled 
-                    and demoralized by the charms of pleasure of the moment, 
-                    so blinded by desire, that they cannot foresee the pain 
-                    and trouble that are bound to ensue; and equal blame belongs to
-                    those who fail in their duty through weakness of will, which is 
-                    the same as saying through shrinking from toil and pain. These 
-                    cases are perfectly simple and easy to distinguish. In a free hour, 
-                    when our power of choice is untrammelled and when nothing prevents 
-                    our being able to do what we like best, every pleasure is to be welcomed 
-                    and every pain avoided. But in certain circumstances and owing to the claims 
-                    of duty or the obligations of business it will frequently occur that pleasures 
-                    have to be repudiated and annoyances accepted. The wise man therefore always holds 
-                    in these matters to this principle of selection: he rejects pleasures to secure other 
-                    greater pleasures, or else he endures pains to avoid worse pains." 
-                    </p>
                 </div>
-            </>
+                <LandingPageIcons />
+                <p>
+                    Check the demo!
+                </p>  
+            </div>
         );
     }
 }
