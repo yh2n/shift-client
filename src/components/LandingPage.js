6568a5c495412f0e9fda01c365094c73c801d896
yh2n <yohann.potico@gmail.com>
2019-06-12 13:01:40 -0400

    Fix alignmnent for smaller screen

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import LandingPageIcons from './LandingPageIcons';
-import Background1 from '../images/blurred-background-calendar-cellphone-1893424.jpg'
+import Background1 from '../images/blurred-background-calendar-cellphone-1893424.jpg';
 
 import './LandingPage.css';
 
@@ -9,10 +9,14 @@ export default class LandingPage extends Component {
     state = {  }
     render() {
         return (
+            <div>
+                <div className="header">shift</div>
             <div className="landing_page">
-                <p>shift</p>
                 <div className="background_images">
                     <img src={Background1} alt="" width="100%" height="100%"/>
+                </div>
+                <div>
+                    Welcome to Shift!!
                 </div>
                 <div className="instructions">
                 </div>
@@ -20,6 +24,7 @@ export default class LandingPage extends Component {
                 <p>
                     Check the demo!
                 </p>  
+            </div>
             </div>
         );
     }
