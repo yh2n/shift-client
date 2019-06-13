d104f76bf1da0e5af126e79253a5d0a91d3931f2
yh2n <yohann.potico@gmail.com>
2019-06-12 20:58:41 -0400

    Add background images for landing page

---
+++ src/components/BackgroundImg.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Background1 from '../images/blurred-background-calendar-cellphone-1893424.jpg';
+import Background2 from '../images/alcohol-bar-bar-cafe-1324896.jpg';
+
+
+// const BackgroundImages = () => {
+//     const imageArr = [Background1, Background2]
+//     for(let i = 0; i ) {
+
+//     }
+// }
