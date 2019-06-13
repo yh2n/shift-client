f43576637158ecedb582e87b48f4c71ed3ce2cc1
yh2n <yohann.potico@gmail.com>
2019-06-13 17:09:57 -0400

    Fix flex alignment

--- src/components/InstructionPage.js
+++ src/components/InstructionPage.js
@@ -18,34 +18,34 @@ export class InstructionPage extends Component {
         })
         return (
             <div className="instruction_page">
-            <div className="instructions">
-                <div className="admin_instructions_div">
-                    <ul>
-                        <li>As an administrator/manager, you can log in using one of the following usernames.</li>
-                        <li>Look at the latest version of your staff availabily to avoid scheduling conflicts.</li>
-                        <li>Create new schedules by clicking on 'Breakfast', 'Lunch', 'Brunch' or 'Dinner'</li>
-                    </ul>
+                <div className="instructions">
+                    <div className="admin_instructions_div">
+                        <ul>
+                            <li>As an administrator/manager, you can log in using one of the following usernames.</li>
+                            <li>Look at the latest version of your staff availabily to avoid scheduling conflicts.</li>
+                            <li>Create new schedules by clicking on 'Breakfast', 'Lunch', 'Brunch' or 'Dinner'</li>
+                        </ul>
+                    </div>
+                    <div className="staff_instructions_div">
+                        <ul>
+                            <li>As an employee, log in using one the following usernames.</li>
+                            <li>Keep your availability up to date.</li>
+                            <li>
+                                Click on the shift you need to get covered and will be displayed red or 
+                                click on the days off you are willing to work and they will be displayed 
+                                green.
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="mock_staff">
+                        <ul>
+                            { usernames }
+                        </ul>
+                    </div>
+                    <div >
+                        <li className="pw_instructions" style={{'listStyle': 'none'}}>Use 'password' for all employees and admin</li>
+                    </div>
                 </div>
-                <div className="staff_instructions_div">
-                    <ul>
-                        <li>As an employee, log in using one the following usernames.</li>
-                        <li>Keep your availability up to date.</li>
-                        <li>
-                            Click on the shift you need to get covered and will be displayed red or 
-                            click on the days off you are willing to work and they will be displayed 
-                            green.
-                        </li>
-                    </ul>
-                </div>
-                <div className="mock_staff">
-                    <ul>
-                        { usernames }
-                    </ul>
-                </div>
-                <div >
-                    <li className="pw_instructions" style={{'listStyle': 'none'}}>Use 'password' for all employees and admin</li>
-                </div>
-            </div>
                 <p className="instructions_page_redirect">
                     <Link to='/login' style={{'textDecoration': 'none'}}>
                         Login | </Link><Link to='/' style={{'textDecoration': 'none'}}>Home </Link>
