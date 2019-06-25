6b845ecde9c485d94c49ccc6dcc8eb7456865899
yh2n <yohann.potico@gmail.com>
2019-06-25 15:09:12 -0400

    Add notification icon to nav and change day color

--- src/components/ContactAvailabilityPage.js
+++ src/components/ContactAvailabilityPage.js
@@ -7,7 +7,12 @@ export default class ContactAvailabilityPage extends Component {
     constructor(props) {
 		super(props);
 
-		this.state = { isOpen : false }
+		this.state = {
+			isOpen : false,
+			availability_alert: false,
+			schedule_alert: false,
+			new_notification: false
+		}
     }	
     
     toggleModal = () => {
@@ -18,7 +23,12 @@ export default class ContactAvailabilityPage extends Component {
         let username = localStorage.getItem('username')
         return (
             <div className="contact_availability_page">
-                <AccountNav onClick={this.toggleModal}/>
+                <AccountNav 
+					onClick={() => this.toggleModal()}
+					className={this.state.new_notification === false ? "material-icons no_notification" : "material-icons new_notification"}
+					markAsRead={this.markAsRead}
+					username={localStorage.getItem('username')}
+				/>
 				<UserMenuModal
 					show={this.state.isOpen}
                     onClose={this.toggleModal}
