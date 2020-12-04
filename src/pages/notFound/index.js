078ca3075c5ce557a36deb97e15147d5679e7c4e
yh2n <yohann.potico@gmail.com>
2020-12-04 18:03:25 -0500

    Fix date and 'not found page'

--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,36 +1,37 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./style.css";
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import "./style.css"
 
 export default class NotFound extends Component {
-    componentDidMount() {
-        document.body.classList.add("not_found_background_color");
-    }
+  componentDidMount() {
+    document.body.classList.add("not_found_background_color")
+  }
 
-    componentWillUnmount() {
-        document.body.classList.remove("not_found_background_color");
-    }
-    render() {
-        return (
-            <div className="not_found_page">
-                <h2 className="not_found_emoji">
-                    <span role="img" aria-label="weary-face">
-                        😩
-                    </span>
-                </h2>
-                <h2 className="not_found_text">
-                    Sorry the page you requested can not be found
-                </h2>
-                <Link
-                    to="/login"
-                    style={{
-                        textDecoration: "none",
-                        color: "rgba(242, 201, 79, 0.9)"
-                    }}
-                >
-                    Back
-                </Link>
-            </div>
-        );
-    }
+  componentWillUnmount() {
+    document.body.classList.remove("not_found_background_color")
+  }
+  render() {
+    return (
+      <div className="not_found_page">
+        <h2 className="not_found_emoji">
+          <span role="img" aria-label="weary-face">
+            😩
+          </span>
+        </h2>
+        <h2 className="not_found_text">
+          Sorry the page you requested can not be found
+        </h2>
+        <h2>Please check your url and try again</h2>
+        <Link
+          to="/login"
+          style={{
+            textDecoration: "none",
+            color: "rgba(242, 201, 79, 0.9)",
+          }}
+        >
+          Back
+        </Link>
+      </div>
+    )
+  }
 }
